e78ffdbdcb320b4f3831b04ce422723670f8a16c
khemroat <khemroat@gmail.com>
2017-08-08 19:08:16 -0700

    [Added]: Maximum Depth Binary Tree

---
+++ src/io/kapp/algo/implementation/easy/MaxDepthBinaryTree.java
@@ -0,0 +1,18 @@
+package io.kapp.algo.implementation.easy;
+
+/**
+ * Find maximum depth of binary tree.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the root
+ * node down to the farthest leaf node.
+ */
+public class MaxDepthBinaryTree {
+	
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+	}
+}
