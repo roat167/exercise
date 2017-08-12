783d75962281a67dca7832c658a01641358cb113
khemroat <khemroat@gmail.com>
2017-08-11 19:31:35 -0700

    [Updated]: TreeNode insert and print method

--- src/io/kapp/algo/implementation/easy/TreeNode.java
+++ src/io/kapp/algo/implementation/easy/TreeNode.java
@@ -1,5 +1,8 @@
 package io.kapp.algo.implementation.easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 //binary tree node.
 class TreeNode {
 	int val;
@@ -8,5 +11,43 @@ class TreeNode {
 
 	TreeNode(int x) {
 		val = x;
-	}	
+	}
+
+	public static TreeNode insert(TreeNode root, int x) {
+		if (root == null) {
+			return new TreeNode(x);
+		} else {
+			TreeNode cur;
+			if (x <= root.val) {
+				cur = insert(root.left, x);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, x);
+				root.right = cur;
+			}
+		}
+		return root;
+	}
+
+	// Using level order traversal
+	public static void printNode(TreeNode root) {
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		if (root != null) {
+			q.add(root);
+		}
+
+		while (!q.isEmpty()) {
+			TreeNode cur = q.remove();
+
+			if (cur.left != null) {
+				q.add(cur.left);
+			}
+
+			if (cur.right != null) {
+				q.add(cur.right);
+			}
+
+			System.out.printf("%d ", cur.val);
+		}
+	}
 }
