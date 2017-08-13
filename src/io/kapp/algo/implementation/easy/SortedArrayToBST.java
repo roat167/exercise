067ca71da18eda1e2ac09924788e17f7469116ef
khemroat <khemroat@gmail.com>
2017-08-12 21:40:15 -0700

    [Added]: Convert sorted array to BST

---
+++ src/io/kapp/algo/implementation/easy/SortedArrayToBST.java
@@ -0,0 +1,30 @@
+package io.kapp.algo.implementation.easy;
+
+public class SortedArrayToBST {
+
+	public static TreeNode sortBST(int[] nums, int low, int high) {
+		if (low > high) {
+			return null;
+		}
+		int mid = (low + high) / 2;
+
+		TreeNode head = new TreeNode(nums[mid]);
+		head.left = sortBST(nums, low, mid - 1);
+		head.right = sortBST(nums, mid + 1, high);
+
+		return head;
+	}
+
+	// Convert Sorted Array to Binary Search Tree 108
+	public static TreeNode sortedArrayToBST(int[] nums) {
+		int len = nums.length;
+		if (len == 0) {
+			return null;
+		}
+		if (len == 1) {
+			return new TreeNode(nums[0]);
+		}
+
+		return sortBST(nums, 0, len - 1);
+	}
+}
